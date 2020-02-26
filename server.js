5ebfe651f220f80197de82e511ff4488ae82c20a
hamid <hamid331994@gmail.com>
2020-02-26 19:09:35 +0330

    dev

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const express = require('express')
+const shortid = require('shortid')
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello from BACKEND')
-  console.log('server running')
+const items = [
+  {
+    id: 1,
+    name: 'book'
+  },
+  {
+    id: 2,
+    name: 'paper'
+  },
+  {
+    id: 3,
+    name: 'tissue'
+  },
+  {
+    id: 4,
+    name: 'moblie'
+  },
+  {
+    id: 5,
+    name: 'carpet'
+  },
+]
+
+app.get('/api/items', (req, res) => {
+  // res.send(items)
+  res.send('items')
+})
+
+app.get('/api/items/:id', (req, res) => {
+  if() {
+
+  }  
+
 })
 
 app.listen(5000)
